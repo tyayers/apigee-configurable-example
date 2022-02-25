4c55d919699010a837232091d4b69cbcabc17610
Tyler Ayers <tyler.ayers@gmail.com>
2022-02-25 08:30:31 +0100

    first example version

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+gcloud alpha apigee archives deploy \
+--organization=bap-emea-apigee-6 \
+--environment=config-dev \
+--source=.
---
+++ test.sh
@@ -0,0 +1,3 @@
+echo $TOKEN
+
+curl -H "Authorization: Bearer $TOKEN" https://dev.api.apigee.cloud10x.net/test -i
